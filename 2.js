3db5f53467aa69af6e63d0d46434a2ed0df96313
satriyawicaksana <satriyawicaksana08@gmail.com>
2020-02-01 21:08:24 +0700

    adding password regex

--- 2.js
+++ 2.js
@@ -2,8 +2,10 @@ const checkUsername = (username) => {
   return /^x{1}[A-Z]{4,7}x{1}$/.test(username);
 }
 const checkPassword = (password) => {
-  // NOT FINISHED YET
+  return /(?=.*[A-Z]{3})(?=.*[@#$%^&+!=])(?=.*[a-z])(?=.*[0-9])/g.test(password);
 }
 
-console.log(checkUsername('xHOHOASDFx'));
-console.log(checkUsername('xHOHOASFx'));
+console.log(checkUsername('xFATHURx'));
+console.log(checkUsername('xRidhox'));
+console.log(checkPassword('kick2XXX!'));
+console.log(checkPassword('aV8V7V6?'));
